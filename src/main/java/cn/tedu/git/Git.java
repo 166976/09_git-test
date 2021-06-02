44742f05364c60d36e21f74e044d4397d1db09c2
yang1 <yang1@tedu.cn>
2021-06-02 17:43:14 +0800

    third commit

--- src/main/java/cn/tedu/git/Git.java
+++ src/main/java/cn/tedu/git/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("helloWorld!!!");
         System.out.println("aaaa");
+        System.out.println("third commit");
     }
 }
